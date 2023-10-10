34bd2e68cea8217e9fa8a96053a5f14e5aed54eb
dc <damnyan@gmail.com>
2023-10-10 14:17:32 +0800

    Add transaction type enum

--- src/CreditFile/Enum/TransactionType.php
+++ src/CreditFile/Enum/TransactionType.php
@@ -5,6 +5,6 @@ namespace Dmn\Lbp\CreditFile\Enum;
 class TransactionType
 {
     public const CREDIT_TO_CREDITORS = '01';
-    public const CREIT_TO_TWO = '02';
-    public const CREIT_TO_THREE = '03';
+    public const PAYROLL = '02';
+    public const BILLS_PAYMENT = '03';
 }
